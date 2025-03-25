bb73ed6b76d57d59df3ead6730c3b60dd64348f4
Tom Hennessy <tomlhennessy@outlook.com>
2025-03-25 11:40:03 +1000

    header and GoTo component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import "./fanta.css";
 import Head from "./head";
+import Link from "next/link";
+import GoTo from "@/components/GoTo";
 
 
 export const metadata = {
@@ -9,11 +11,35 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const header = (
+    <header>
+      <div>
+        <Link href={'/'}>
+          <h1 className='text-gradient'>Expenny</h1>
+        </Link>
+        <p>The Subscription Tracker</p>
+      </div>
+      <GoTo />
+    </header>
+  )
+
+  const footer = (
+    <footer>
+      <div className='hard-line' />
+    </footer>
+  )
+
   return (
     <html lang="en">
       <Head />
       <body>
-        {children}
+        {header}
+        <div className='full-line' />
+        <main>
+          {children}
+        </main>
+        {footer}
       </body>
     </html>
   );
--- components/GoTo.jsx
+++ components/GoTo.jsx
@@ -1,6 +1,28 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function GoTo() {
+    const isAuthenticated = false
+
+    const path = usePathname()
+
     return (
-      <div></div>
+      <div className='goto'>
+        {path == '/' && (
+          <>
+            <Link href={'/dashboard?register=true'}>
+              <p>Sign Up</p>
+            </Link>
+            <Link href={'/dashboard'}>
+              <button>Login &rarr;</button>
+            </Link>
+          </>
+        )}
+        {(isAuthenticated && path == '/dashboard') && (
+          <button>Logout</button>
+        )}
+      </div>
     )
   }
