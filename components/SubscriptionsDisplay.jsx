729ff1deae53a789fd38fdb14c6a7f4f0c6b3032
Tom Hennessy <tomlhennessy@outlook.com>
2025-03-27 10:22:22 +1000

    subscription display

--- components/SubscriptionsDisplay.jsx
+++ components/SubscriptionsDisplay.jsx
@@ -1,7 +1,37 @@
+import { subscriptions } from "@/utils"
 
 
 export default function SubscriptionsDisplay() {
     return (
-      <div></div>
+      <section>
+        <h2>Your Subcriptions</h2>
+        <div className='card-container'>
+          {subscriptions.map((sub, subIndex) => {
+            const { name, category, cost, currency, billingFrequency, startDate, notes, status } = sub
+
+            return (
+              <div key={subIndex} className='card subcription-card'>
+                <div>
+                  <h3>{name}</h3>
+                  <div className={'status ' + (status === 'Active' ? 'card-button-primary' : 'card-button-secondary')}>
+                    <small>{status}</small>
+                  </div>
+                </div>
+
+                <p><i>{category}</i></p>
+
+                <div className='sub-cost'>
+                  <h2>${cost}</h2>
+                  <p>{currency}</p>
+                </div>
+
+                <small>per {billingFrequency}</small>
+
+              </div>
+
+            )
+          })}
+        </div>
+      </section>
     )
   }
